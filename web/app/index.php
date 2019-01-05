6979006f680e5d11b1a19746e8ed419a8d129c80
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2019-01-05 23:33:07 +0100

    Move source to app directory
    to follow conventional structure

---
+++ web/app/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'utils/session.php';
+require 'utils/redirections.php';
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if (!$is_session_active && $uri != '/login') {
+    redirectTo('/login');
+}
+
+if ($is_session_active && $uri == '/login') {
+    redirectTo('/');
+}
+
+switch ($uri) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/login':
+        require __DIR__ . '/views/login.php';
+        break;
+    default:
+        require __DIR__ . '/views/404.php';
+        break;
+}
+
+?>
